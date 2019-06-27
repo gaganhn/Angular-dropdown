a614f95dd970cd31b16299e2f0c5b26040d19cc9
Gagan H N <gagan.hn@softwaysolutions.com>
2019-06-28 00:41:05 +0530

    FEAT - LOGIC PART(WIP)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   dropdownActive: Boolean = false;
+  options: any = [
+    {
+      "avatar": "assets/images/img_avatar_male.png",
+      "name": "John",
+      "email": "john@example.com"
+    },
+    {
+      "avatar": "assets/images/img_avatar_female.png",
+      "name": "Jane",
+      "email": "jane@example.com"
+    },
+    {
+      "avatar": "assets/images/img_avatar_male.png",
+      "name": "Joker",
+      "email": "joker@example.com"
+    },
+  ];
+  selectedItems: any = [
+    {
+      "avatar": "assets/images/img_avatar_male.png",
+      "name": "Batman",
+      "email": "batman@example.com"
+    }
+  ];
 
   constructor() {
 
   }
 
-  
 }
